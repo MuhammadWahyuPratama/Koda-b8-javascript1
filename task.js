159049337f6586488482e824397f329ab58a472d
MuhammadWahyuPratama <muhammad123wahyu123@gmail.com>
2026-05-13 13:13:36 +0700

    Add files via upload

---
+++ task.js
@@ -0,0 +1,69 @@
+// Membuat program untuk memunculkan string "Koda" ketika mengakses objek menubar.are.the.best
+
+const we = {
+    are: {
+        the: {
+            best: "koda"
+        }
+    }
+}
+
+console.log(we.are.the.best)
+
+
+// Membuat program untuk memunculkan string "Hello World" ketika mengakses objek hello.world
+
+const hello = {
+    world: "Hello World"
+}
+
+console.log(hello.world)
+
+//Membuat Program untuk memunculkan string "Tech Academy" ketika megakses objek obj.str[3][1][2].man[0].tech.academy
+
+const obj = {
+    str: [, , ,
+        [,
+            [, ,
+                {
+                    man: [
+                        {
+                            tech: {
+                                academy: "Tech Academy"
+                            }
+                        }
+                    ]
+                }
+            ]
+        ]
+    ]
+}
+
+console.log(obj.str[3][1][2].man[0].tech.academy)
+
+// Membuat program untuk memunculkan string "Apple" ketika mengakses array my[0].favourite[3].fruit.is
+
+const my = [
+    {
+        favourite: [
+            , , , {
+                fruit: {
+                    is: "Apple"
+                }
+            }
+        ]
+    }
+]
+
+console.log(my[0].favourite[3].fruit.is)
+
+//Memunculkan Number 32 ketika kalkulasi num.first[1]+ num.second[2]
+
+const num = {
+    first: [,
+        16
+    ],
+    second: [, , 16]
+}
+
+console.log(num.first[1] + num.second[2])
