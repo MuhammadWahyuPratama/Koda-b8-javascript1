7ce9479edc6889c0ff730e05304df235edf3be8d
MuhammadWahyuPratama <muhammad123wahyu123@gmail.com>
2026-05-13 10:39:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const r = "12"
-let pI = 22.7
-let luasLingkaran = pI * r * r
-let kelilingLingkaran = 2 * pI * r
+// Membuat program hitung luas dan keliling lingkaran
+
+const r = 12
+const pI = 22.7
+const luasLingkaran = pI * r * r
+const kelilingLingkaran = 2 * pI * r
 
 
 console.log(luasLingkaran)
