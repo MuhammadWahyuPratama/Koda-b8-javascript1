0a1cc2f43e5f65d279dfdaf50a6765a918070de8
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-05-15 14:08:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 // Membuat program hitung luas dan keliling lingkaran
 
-const r = 21
-let pi
 
-if (typeof r !== "number") {
-    console.log("r must be a number")
-    return
+function sum(r) {
+    let pi
+    if (typeof r !== "number") {
+        console.log("r must be a number")
+        return
+    }
+
+
+    if (r % 22 / 7 == 0) {
+        pi = 22 / 7
+    } else {
+        pi = 3.14
+    }
+
+
+    const luasLingkaran = pi * r * r
+    const kelilingLingkaran = 2 * pi * r
+
+
+    console.log(luasLingkaran)
+    console.log(kelilingLingkaran)
 }
 
-
-if (r % 22 / 7 == 0) {
-    pi = 22 / 7
-} else {
-    pi = 3.14
-}
-
-
-const luasLingkaran = pi * r * r
-const kelilingLingkaran = 2 * pi * r
-
-
-console.log(luasLingkaran)
-console.log(kelilingLingkaran)
+sum(7)
